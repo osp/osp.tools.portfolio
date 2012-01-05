45e856fa8c92d025cf2092ee0f6511887f823e24
Eric Schrijver <eric@ericschrijver.nl>
2012-01-05 19:01:12 +0100

    Add the image listing to the markdown recipe
    The images themselves not downloaded yet though

--- make.sh
+++ make.sh
@@ -2,7 +2,7 @@
 
 projects=('balsamine' 'disappearance')
 url="http://git.constantvzw.org/?p=%s.git;a=blob_plain;f=README"
-
+index="http://git.constantvzw.org/?p=%s.git;a=blob_plain;f=iceberg/index.mkd"
 
 mkdir -p tmp
 cd tmp
@@ -11,6 +11,7 @@ cd tmp
 for project in "${projects[@]}"
 do 
     wget -O"${project}.mkd" $(printf ${url} ${project})
+    wget -O - >> $(printf ${index} ${project}) "${project}.mkd"
     pandoc -f markdown -t context -o "${project}.tex" "${project}.mkd"
     echo -e "\n\n% END OF ${project}\n\n" >> "${project}.tex"
 done
