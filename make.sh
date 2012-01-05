f058069e412051a0564a2a6c02e894155aade395
Eric Schrijver <eric@ericschrijver.nl>
2012-01-05 19:30:30 +0100

    Better README

--- make.sh
+++ make.sh
@@ -1,6 +1,7 @@
 #! /usr/bin/env bash
 
-projects=('balsamine' 'disappearance')
+# projects=('balsamine' 'disappearance')
+projects=('balsamine')
 url="http://git.constantvzw.org/?p=%s.git;a=blob_plain;f=README"
 index="http://git.constantvzw.org/?p=%s.git;a=blob_plain;f=iceberg/index.mkd"
 
@@ -9,9 +10,14 @@ cd tmp
 
 
 for project in "${projects[@]}"
-do 
+do
+    # Get the README
     wget -O"${project}.mkd" $(printf ${url} ${project})
+    # Just the introductory explanation:
+    python -c "f = open('${project}.mkd', 'r+'); b = f.read().split('- - -')[0]; f.write(b) "
+    # Add image list:
     wget -O - >> $(printf ${index} ${project}) "${project}.mkd"
+    # To tex:
     pandoc -f markdown -t context -o "${project}.tex" "${project}.mkd"
     echo -e "\n\n% END OF ${project}\n\n" >> "${project}.tex"
 done
